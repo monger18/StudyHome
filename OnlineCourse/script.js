15bede40eee98457fbd935dddf7a41066540a299
Satyam srinath <130518559+monger18@users.noreply.github.com>
2023-07-18 03:25:20 +0530

    Add files via upload

---
+++ OnlineCourse/script.js
@@ -0,0 +1,40 @@
+
+window.addEventListener('scroll',()=>
+{
+   document.querySelector('nav').classList.toggle('window-scroll',window.scrollY >0)   
+} )
+
+
+//show or hide faq
+const faqs=document.querySelectorAll('.faq');
+faqs.forEach(faq=>{
+   faq.addEventListener('click',()=>{
+      faq.classList.toggle('open');
+      //change icon
+      const icon=faq.querySelector('.faq_icon i');
+      if(icon.className==='uil uil-plus'){
+      icon.className="uil uil-minus"
+      }
+      else{
+         icon.className="uil uil-plus"
+      }
+   })
+})
+
+
+const menu=document.querySelector(".nav__menu");
+const menubtn=document.querySelector("#open-menu-btn");
+const closebtn=document.querySelector("#close-menu-btn");
+
+menubtn.addEventListener('click',()=>{
+   menu.style.display="flex";
+   closebtn.style.display="inline-block";
+   menubtn.style.display="none";
+})
+
+const closenav=()=>{
+   menu.style.display="none";
+   closebtn.style.display="none";
+   menubtn.style.display="inline-block";
+}
+closebtn.addEventListener('click',closenav);
